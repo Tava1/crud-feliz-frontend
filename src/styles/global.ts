b7f560bc5554d6ac8fdd9daf0f8d104c94654873
Gustavo <gusta99sant@gmail.com>
2021-04-23 17:53:39 -0300

    chore: Project structure

---
+++ src/styles/global.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+@media(max-width: 1080px) {
+  html {
+    font-size: 93.75%; 
+  }
+}
+
+@media(max-width: 720px) {
+  html {
+    font-size: 87.5%; 
+  }
+}
+
+body {
+  background: #FCFCFC;
+  color: #171717;
+}
+
+body, button {
+  font: 400 1rem Roboto, sans-serif;
+}
+
+button {
+  cursor: pointer;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul {
+  list-style-type: none;
+}
+
+`;
