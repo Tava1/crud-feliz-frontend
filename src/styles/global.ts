179d16fea8e55f5819b028c81e0bcff799e9c5af
Gustavo <gusta99sant@gmail.com>
2021-04-24 23:01:09 -0300

    style: Create global grid

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,44 +1,52 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-@media(max-width: 1080px) {
-  html {
-    font-size: 93.75%; 
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
-}
 
-@media(max-width: 720px) {
-  html {
-    font-size: 87.5%; 
+  @media(max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
   }
-}
 
-body {
-  background: #FCFCFC;
-  color: #171717;
-}
+  @media(max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
 
-body, button {
-  font: 400 1rem Roboto, sans-serif;
-}
+  body {
+    background: #FCFCFC;
+    color: #171717;
+    height: 100%;
+  }
 
-button {
-  cursor: pointer;
-}
+  body, button {
+    font: 400 1rem Roboto, sans-serif;
+  }
 
-a {
-  color: inherit;
-  text-decoration: none;
-}
+  button {
+    cursor: pointer;
+  }
 
-ul {
-  list-style-type: none;
-}
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-rows: auto;
+    grid-gap: 10px;
+  }
 
 `;
